834815350129efb73fa300459825db07cf86b7c7
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-07 22:28:12 +0530

    feat: quiz types

---
+++ src/types/quiz/quiz.types.ts
@@ -0,0 +1,45 @@
+export type QuizAction = {
+	type: string;
+	payload?: any;
+};
+
+export type QuizDispatch = (action: QuizAction) => void;
+
+export type Option = {
+	_id: string;
+	option: string;
+	isCorrect: boolean;
+};
+
+export type Question = {
+	_id: string;
+	question: string;
+	options: Option[];
+};
+
+export type SelectedOption = {
+	option: string;
+	_id: string;
+};
+
+export type Category = {
+	categoryId: string;
+	categoryName: string;
+};
+
+export type QuizState = {
+	quizName: string;
+	category: Category;
+	quizDataLoading: boolean;
+	quizDataError: null | string;
+	quizId: string;
+	questions: Question[];
+	selectedOptions: any[];
+	currentQuestionNumber: number;
+	totalScore: number;
+};
+
+export type QuizContextType = {
+	quizState: QuizState;
+	quizDispatch: QuizDispatch;
+};
