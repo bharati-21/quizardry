f7d19a4e70218eb727e0a0d4d42bc550bbdfd358
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-16 15:11:20 +0530

    test: post new quiz, delete quiz

---
+++ src/test/quiz-services/deleteItemFromQuizService.test.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { deleteItemFromQuizService } from "services";
+
+jest.mock("axios");
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+const quizId = "62c2dc868276e7ba8f771f24";
+
+const response = {
+	data: {
+		message: "Deleted quiz successfully!",
+		quizzes: [],
+	},
+};
+
+describe("Delete quiz item service API call", () => {
+	describe("Positive test", () => {
+		beforeEach(() => {
+			(axios.delete as jest.Mock).mockResolvedValue(response);
+		});
+
+		it("Should return the correct response", async () => {
+			const {
+				data: { quizzes },
+			} = await deleteItemFromQuizService(token, quizId);
+
+			expect(quizzes).toHaveLength(0);
+			expect(quizzes).toEqual([]);
+		});
+		it("Should call the correct API service", async () => {
+			await deleteItemFromQuizService(token, quizId);
+			expect(axios.delete).toBeCalledWith(
+				`https://quizardry-server.herokuapp.com/api/quizzes/${quizId}`,
+				{ headers: { authorization: token } }
+			);
+		});
+	});
+	describe("Negaive test", () => {
+		it("Should return the error with message: 404 Selected quiz id not found.", async () => {
+			(axios.delete as jest.Mock).mockRejectedValue({
+				message: "404 Selected quiz id not found.",
+			});
+
+			expect.assertions(1);
+			await expect(
+				deleteItemFromQuizService(token, "")
+			).rejects.toStrictEqual({
+				message: "404 Selected quiz id not found.",
+			});
+		});
+
+		it("Should return the error with message: 401: Authorization error. Invalid token.", async () => {
+			(axios.delete as jest.Mock).mockRejectedValue({
+				message: "401 Authorization Error! Invalid token.",
+			});
+			expect.assertions(1);
+			await expect(
+				deleteItemFromQuizService("", quizId)
+			).rejects.toStrictEqual({
+				message: "401 Authorization Error! Invalid token.",
+			});
+		});
+	});
+});
